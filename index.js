e2b3c3bb5720405b9c2e299a1a5d6357c0171ae1
KimPlayz4LK <leokim060109@gmail.com>
2020-09-01 09:17:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 require('dotenv').config()
 const prefix = process.env.prefix;
-const token = process.env.token;
+const token = "NzQ0NTU1MzcwNzg5MzM5MTU2.Xzk7OQ.t57lLvb_ZbhuhwDxKVBMm2v0dmA";
 function between(min, max) {return Math.floor(Math.random() * (max - min) + min)}
 function newStatus() {
 if (between(0,10) > 4) {
@@ -20,7 +20,7 @@ if (user.bot===false) {
 if (reaction.partial) {
 try {await reaction.fetch();}
 catch (error) {console.log('Something went wrong when fetching the message: ', error);return;}}
-if (reaction.message.content==="^ticket") {
+if (reaction.message.content.startsWith("^ticket")===true) {
 const userReactions = reaction.message.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
 try {for (const reaction of userReactions.values()) {await reaction.users.remove(user.id);}} catch (error) {console.error("Failed to remove reactions");}
 var server = reaction.message.guild;
@@ -35,15 +35,18 @@ message.react("🎟");
 const embed = new Discord.MessageEmbed()
 .setColor("#0099ff")
 .setTitle(":tickets: | Ticket")
-.setDescription("React to the message above to create a ticket")
+.setDescription("React to this message to create a ticket")
 .setFooter("Ticket bot");
-message.channel.send(embed);
+message.channel.send(embed)
+.then(function(message) {
+message.react("🎟");
+}).catch(function(){});
 }
 if (command.startsWith("invite")===true){
     const embed = new Discord.MessageEmbed()
     .setColor("#0099ff")
     .setTitle(":envelope: | Invite")
-    .setDescription("Here's an invite link for the bot: https://discord.com/api/oauth2/authorize?client_id=744555370789339156&permissions=8&scope=bot")
+    .setDescription("Here's an invite link for the bot: [Invite link](https://discord.com/api/oauth2/authorize?client_id=744555370789339156&permissions=8&scope=bot)")
     .setFooter("Ticket bot");
     message.channel.send(embed);
     }
